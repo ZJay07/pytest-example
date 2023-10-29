2d788cf1c92bc99b3802c7fe68ea9b943f7889bb
Jay Choy <zcabzjc@ucl.ac.uk>
2023-10-29 13:25:07 +0000

    fixtures

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,12 +1,28 @@
 import pytest
 import source.shapes as shapes
+import math
+
 
 class TestCircle:
 
     def setup_method(self, method):
         print(f"Setting up {method}")
+        self.circle = shapes.Circle(10)
 
     def teardown_method(self, method):
         print(f"Tearing down {method}")
-    def test_one(self):
-        assert True
+        del self.circle
+
+    def test_radius(self):
+        assert self.circle.radius
+
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+
+    def test_perimeter(self):
+        result = self.circle.perimeter()
+        expectedValue = 2 * math.pi * self.circle.radius
+        assert result == expectedValue
+
+    def test_not_same_area_rectangle(self, my_rectangle):
+        assert self.circle.area() != my_rectangle.area()
